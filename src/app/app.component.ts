c45e21f9ce48ab2e3eb3daf540c22ca14172f3b2
Max Korsakov <Maksim_Karsakou1epam.com>
2021-03-09 18:53:31 +0300

    change storage to map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { InMemoryCache } from './inmemory-cache';
+import { Component, OnInit } from '@angular/core';
+import { InMemoryCache } from 'angular-caching/inmemory-cache';
 import { of } from 'rxjs';
 
 @Component({
@@ -10,22 +10,38 @@ import { of } from 'rxjs';
 export class AppComponent {
   title = 'caching';
 
-  public result
-  public result2
-  public result3
+  public result;
+  public result2;
+  public result3;
+
+  public result4;
+  public result5;
+  public result6;
 
   public a;
   public b;
+  public c;
+  public d;
 
-  public setData(){
-    this.getSum(this.a, this.b).subscribe(result => this.result = result)
-    this.getSum(this.a, this.b).subscribe(result => this.result2 = result)
-    this.getSum(this.a, this.b).subscribe(result => this.result3 = result)
+  public setData() {
+    this.getSum(this.a, this.b).subscribe((result) => (this.result = result));
+    this.getSum(this.a, this.b).subscribe((result) => (this.result2 = result));
+    this.getSum(this.a, this.b).subscribe((result) => (this.result3 = result));
   }
 
-  @InMemoryCache()
-  public getSum(a: number, b: number) {
+  public setDataPrim() {
+    this.result4 = this.getSumPrim(this.c, this.d);
+    this.result5 = this.getSumPrim(this.c, this.d);
+    this.result5 = this.getSumPrim(this.c, this.d);
+  }
+
+  @InMemoryCache({sync: false})
+  private getSum(a: number, b: number) {
     return of(Number(a) + Number(b));
   }
 
+  @InMemoryCache({ ttl: 3000, cacheSize: 2 })
+  private getSumPrim(a: number, b: number) {
+    return Number(a) + Number(b);
+  }
 }
--- src/app/inmemory-cache.ts
+++ src/app/inmemory-cache.ts
@@ -4,16 +4,17 @@ import { publishReplay, refCount, take } from 'rxjs/operators';
 export interface InMemoryCacheOptions {
   ttl?: number;
   cacheSize?: number;
+  sync?: boolean;
 }
+
+type CachedData = Promise<any> | Observable<any> | any;
 
 export function InMemoryCache(params: InMemoryCacheOptions = {}) {
   let originalFunc: Function;
   const cacheSize = params.cacheSize || 1;
-
-  let cache: {
-    [key: string]: Promise<any> | Observable<any> | any;
-  }[] = [];
-  const timers = {};
+  const chacheTTL = params.ttl;
+  const cacheMap = new Map<string, CachedData>();
+  const timersMap = new Map<string, ReturnType<typeof setTimeout>>();
 
   return function (
     target: any,
@@ -23,41 +24,43 @@ export function InMemoryCache(params: InMemoryCacheOptions = {}) {
     originalFunc = descriptor.value;
 
     descriptor.value = function (...args) {
-      const id = JSON.stringify(args);
-      const currentCacheSize = Object.keys(cache).length;
-      const data =
-        cache.find((cachedItem) => cachedItem[id]) ||
-        ({} as { [key: string]: Promise<any> | Observable<any> | any });
-      /// Console for test purpouse ///
-      if (!data[id]) {
-        console.log('From origin');
-      } else {
-        console.log('From cash');
+      if (!args.length && cacheSize > 1) {
+        throw new Error(
+          `If you don't provide arguments, cache size could not be bigger then 1`
+        );
       }
+      const id = JSON.stringify(args);
+      const currentCacheSize = cacheMap.size;
+      let data = cacheMap.get(id);
+      if (!data) {
+        data = originalFunc.apply(this, args);
 
-      if (!data[id]) {
-        data[id] = originalFunc.apply(this, args);
-
-        if (data[id] instanceof Observable) {
-          data[id] = data[id].pipe(publishReplay(1), refCount(), take(1));
+        if (data instanceof Observable) {
+          data = data.pipe(publishReplay(1), refCount(), take(1));
         }
         if (currentCacheSize >= cacheSize) {
-          if (timers[Object.keys(cache[0])[0]]) {
-            clearTimeout(timers[Object.keys(cache[0])[0]]);
-            delete timers[Object.keys(cache[0])[0]];
+          if (timersMap.has(cacheMap.keys()[0])) {
+            clearTimeout(timersMap.get(cacheMap.keys()[0]));
+            timersMap.delete(cacheMap.keys()[0]);
           }
-          cache.shift();
+          cacheMap.delete(cacheMap.keys()[0]);
         }
-        cache.push(data);
+        if (params.sync) {
+          cacheMap.set(id, data);
+        } else {
+          Promise.resolve().then(() => {
+            cacheMap.set(id, data);
+          });
+        }
+        if (chacheTTL || chacheTTL === 0) {
+          const timerId = setTimeout(() => {
+            cacheMap.delete(id);
+            timersMap.delete(id);
+          }, chacheTTL);
+          timersMap.set(id, timerId);
+        }
       }
-
-      if (params.ttl) {
-        timers[id] = setTimeout(() => {
-          cache = cache.filter((item) => Object.keys(item)[0] !== id);
-          delete timers[id];
-        }, params.ttl);
-      }
-      return data[id];
+      return data;
     };
   };
 }
